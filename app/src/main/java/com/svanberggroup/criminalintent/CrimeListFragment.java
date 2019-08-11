fa9fa29d9dbf176a64cb7a30fb6929c0431946b9
Glenn Svanberg <signeratsvanberg@gmail.com>
2019-08-11 13:34:26 +0200

    Empty CrimeListActivityScreen (Not working)

---
+++ app/src/main/java/com/svanberggroup/criminalintent/CrimeListFragment.java
@@ -0,0 +1,16 @@
+package com.svanberggroup.criminalintent;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class CrimeListFragment extends Fragment{
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    
+}
