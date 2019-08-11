bc75c430293eaf1f974e80f3b188e2bf3f7e31e3
Glenn Svanberg <signeratsvanberg@gmail.com>
2019-08-11 14:44:53 +0200

    OnClick Recyclerview

--- app/src/main/java/com/svanberggroup/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/svanberggroup/criminalintent/CrimeListFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -41,7 +42,7 @@ public class CrimeListFragment extends Fragment{
         mCrimeRecyclerView.setAdapter(mAdapter);
     }
 
-    private class CrimeHolder extends RecyclerView.ViewHolder {
+    private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         private TextView mTitleTextView;
         private TextView mDateTextView;
@@ -49,9 +50,15 @@ public class CrimeListFragment extends Fragment{
 
         public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.list_item_crime,parent,false));
+            itemView.setOnClickListener(this);
 
             mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
             mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getActivity(), mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT).show();
         }
 
         public void bind(Crime crime) {
